8b5f316a4c0100c85b0eb84318c96e9d0d4d0c48
GaurangDhorda <dg.dhorda@gmail.com>
2019-07-16 14:47:44 +0530

    adding firebase support

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ app.use(bodyParser.json());
 app.use (cors());
 
 var path = require('path');
+
+// firebase coding... 
+var firebase = require('firebase');
+var config = {
+	apiKey: 'AIzaSyA8hO5x0WDnFsbdMG2iSJIxNsCywGevOfk',
+	authDomain: 'testingapp-8fb86.firebaseapp.com',
+	databaseURL: 'https://testingapp-8fb86.firebaseio.com',
+	projectId: 'testingapp-8fb86',
+	storageBucket: 'testingapp-8fb86.appspot.com',
+	messagingSenderId: '345026705484',
+	appId: '1:345026705484:web:962e5b869e82de52'
+};
+firebase.initializeApp(config);
+
 // when url fire by client-side this app.get('/') default requenst fire and hello.html response fire..
 app.get('/', (req, res) => {
 	//	res.send('Hello World, Response from express server');
@@ -24,10 +38,14 @@ app.get('/', (req, res) => {
 	//	res.status(301).redirect('https://gaurangdhorda.github.io/Angular-Demo/');
 	
 	});
-
+  
 app.post('/enroll',(req, res) => {
+	// enroll is called from angular app and data are loaded here.. then we save this data to the firebase database...
 	res.send(req.body);
+	firebase.database().ref('/Material-Contact').set(req.body);
 	res.status (200).send({'msg': 'Data received'});
+
+	
 })
 
 io.on('connection', (socket) => {
